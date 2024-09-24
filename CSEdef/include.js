b70b9f7320f319bc6ecb1c3272f1edee754d3bc9
Abu Salah Musha Lemon <lemonahmed512@gmail.com>
2024-09-24 14:24:33 +0600

    update

--- CSEdef/include.js
+++ CSEdef/include.js
@@ -1,11 +1,31 @@
+fetch('./include/head.html')
+  .then(response => response.text())
+  .then(content => {
+    document.getElementById('head').innerHTML = content;
+  })
 
-fetch('nav.html')
+
+fetch('./include/nav.html')
   .then(response => response.text())
   .then(content => {
     document.getElementById('navigation').innerHTML = content;
-  });
-fetch('footer.html')
+  })
+
+
+fetch('./include/footer.html')
   .then(response => response.text())
   .then(content => {
     document.getElementById('footer').innerHTML = content;
-  });
+  })
+
+
+// Update the path based on the correct location of script.html
+fetch('./include/script.html')
+    .then(response => {
+        if (!response.ok) throw new Error('Network response was not ok');
+        return response.text();
+    })
+    .then(content => {
+        document.getElementById('script').innerHTML = content;
+    })
+    .catch(error => console.error('Error fetching script:', error));
